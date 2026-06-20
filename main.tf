2b8d725621b07ecb47272f1a545e15cb6f12ff9c
KuruvaBasavaraja <kuruva.basavaraja09@gmail.com>
2026-06-20 12:54:13 +0530

    update

--- main.tf
+++ main.tf
@@ -68,6 +68,12 @@ resource "aws_db_subnet_group" "rds_snet_group" {
 
 resource "aws_eip" "nat" {
   domain = "vpc"
+  tags = merge(
+  var.common_tags,
+    {
+      Name = "${local.resource_name}-nat-eip"
+    }
+  )
 }
 
 resource "aws_nat_gateway" "natgateway" {
